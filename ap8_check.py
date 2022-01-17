888c6824d9b1f33c49f8f2375531c28e710e81ab
Davide Ragazzon <davide_ragazzon@mckinsey.com>
2022-01-18 00:20:13 +0100

    Understand issues with autopep8 pre-commit config

--- ap8_check.py
+++ ap8_check.py
@@ -1,4 +1,13 @@
 """ Making a very long comment line to see what happens with autopep8 and if it does something when the line is too long.
 Here another line just to be sure.
-Adding another long line now to check if installing pre-commit was the thing to do which I forgot. Let's make this line a bit longer now.
+Adding another long line now to check if installing pre-commit was the thing to do which I forgot.
+ Let's make this line a bit longer now.
 """
+
+import os
+import sys
+import sklearn
+
+
+long_code_line = 1 + 3 + 8 + 9 + 1 + 3 + 4 + 5 + 6 + 7 + 1 + 3 + 4 + 5 + 6 + \
+    7 + 8 + 9 + 1 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 1 + 3 + 4 + 5 + 6 + 7 + 8 + 9
